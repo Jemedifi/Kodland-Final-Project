2bf40e44b7023fa738b838fd4913d7e0d21e25da
Jemedifi <jemedifi@gmail.com>
2025-06-22 22:06:24 -0400

    Add files via upload

--- Project/main.py
+++ Project/main.py
@@ -2,10 +2,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/calc", methods=["GET", "POST"])
 def home():
-    return render_template("base.html",)
-
+    display_result = ""
+    if request.method == "POST":
+        js_variable = request.form.get('jsVariable')
+        try:
+            if all(char.isdigit() or char in '+-*/.%()' for char in js_variable):
+                display_result = str(eval(js_variable))
+            else:
+                display_result = "Error: Invalid Input"
+        except (SyntaxError, TypeError, ZeroDivisionError):
+            display_result = "Error"
+    return render_template("base.html", display_result=display_result)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
